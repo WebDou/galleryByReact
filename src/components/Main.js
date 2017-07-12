3607c42458a7d9fa7e585ed1668b2a0066f2468d
pengdan <dan.peng@mljr.com>
2017-07-12 15:38:24 +0800

    init project

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,20 +3,33 @@ require('styles/App.css');
 
 import React from 'react';
 
-let yeomanImage = require('../images/yeoman.png');
+//获取图片相关的数据
+var imageDatas = require('../data/imageDatas.json');
 
-class AppComponent extends React.Component {
+//利用自执行函数，将图片名信息转成图片url路径信息
+imageDatas = (function genImageUrl(imageDatasArr){
+  for (var i=0;i<imageDatasArr.length;i++){
+    var singleImageData = imageDatasArr[i];
+    singleImageData.imageURL = require('../images/' + singleImageData.fileName);
+    imageDatasArr[i] = singleImageData;
+  }
+  return imageDatasArr;
+})(imageDatas);
+
+class GalleryByReactApp extends React.Component {
   render() {
     return (
-      <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
-      </div>
+      <section className="stage">
+        <section className="img-sec">
+        </section>
+        <nav className="controller-nav">
+        </nav>
+      </section>
     );
   }
 }
 
-AppComponent.defaultProps = {
+GalleryByReactApp.defaultProps = {
 };
 
-export default AppComponent;
+export default GalleryByReactApp;
